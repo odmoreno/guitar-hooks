c94a61797502fc7b71a2521db2473a59b5e5104c
oscar <oscar.moreno@cti.espol.edu.ec>
2024-08-06 01:52:45 -0500

    first

---
+++ src/reducers/cart-reducer.ts
@@ -0,0 +1,77 @@
+import { db } from "../data/db"
+import { CartItem, Guitar } from "../types"
+
+
+export type CartActions =
+    { type: 'add-to-cart', payload: { item: Guitar } } |
+    { type: 'remove-from-cart', payload: { id: Guitar['id'] } } |
+    { type: 'decrease-quantity', payload: { id: Guitar['id'] } } |
+    { type: 'increase-quantity', payload: { id: Guitar['id'] } } |
+    { type: 'clear-cart' }
+
+
+export type CartState = {
+    data: Guitar[],
+    cart: CartItem[]
+}
+
+export const initialState: CartState = {
+    data: db,
+    cart: []
+}
+
+const MIN_ITEMS = 1
+const MAX_ITEMS = 5
+
+export const cartReducer = (
+    state: CartState = initialState,
+    action: CartActions
+) => {
+    if (action.type === "add-to-cart") {
+
+        const itemExists = state.cart.findIndex(guitar => guitar.id === action.payload.item.id)
+
+        let updatedCart: CartItem[] = []
+
+        if (itemExists >= 0) { // existe en el carrito
+            if (state.cart[itemExists].quantity >= MAX_ITEMS) return
+            updatedCart = [...state.cart]
+            updatedCart[itemExists].quantity++
+        } else {
+            const newItem: CartItem = { ...action.payload.item, quantity: 1 }
+            updatedCart = [...state.cart, newItem]
+        }
+
+        return {
+            ...state,
+            updatedCart
+        }
+    }
+
+    if (action.type === "remove-from-cart") {
+        return {
+            ...state
+        }
+    }
+
+    if (action.type === "decrease-quantity") {
+        return {
+            ...state
+        }
+    }
+
+    if (action.type === "increase-quantity") {
+        return {
+            ...state
+        }
+    }
+
+    if (action.type === "clear-cart") {
+        return {
+            ...state
+        }
+    }
+
+    return state
+}
+
