5a5fcf20bd4389ff278731181e2f4085e8a3b91b
Oscar Moreno <oscar.moreno@cti.espol.edu.ec>
2024-08-06 11:55:02 -0500

    implementando use reducer

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,22 @@
-import { useReducer } from "react"
+import { useEffect, useReducer } from "react"
 import Guitar from "./components/Guitar"
 import Header from "./components/Header"
-import { useCart } from './hooks/useCart'
 import { cartReducer, initialState } from "./reducers/cart-reducer"
 
 
 function App() {
 
-  const {cart, addToCart, removeFromCart, decreaseQuantity, increaseQuantity, clearCart, isEmpty, cartTotal } = useCart()
-
   const [state, dispatch] = useReducer(cartReducer, initialState)
   
+  useEffect(() => {
+    localStorage.setItem("cart", JSON.stringify(state.cart))
+  }, [state.cart])
+
   return (
     <>
       <Header 
-        cart={cart}
-        removeFromCart={removeFromCart}
-        decreaseQuantity={decreaseQuantity}
-        increaseQuantity={increaseQuantity}
-        clearCart={clearCart}
-        isEmpty={isEmpty}
-        cartTotal={cartTotal}
+        cart={state.cart}
+        dispatch={dispatch}
       />
       
       <main className="container-xl mt-5">
--- src/reducers/cart-reducer.ts
+++ src/reducers/cart-reducer.ts
@@ -15,63 +15,105 @@ export type CartState = {
     cart: CartItem[]
 }
 
+const initialCart = (): CartItem[] => {
+	const localStorageCart = localStorage.getItem("cart")
+	return localStorageCart ? JSON.parse(localStorageCart) : []
+}
+
 export const initialState: CartState = {
-    data: db,
-    cart: []
+	data: db,
+	cart: initialCart(),
 }
 
 const MIN_ITEMS = 1
 const MAX_ITEMS = 5
 
 export const cartReducer = (
-    state: CartState = initialState,
-    action: CartActions
+	state: CartState = initialState,
+	action: CartActions
 ) => {
-    if (action.type === "add-to-cart") {
+	if (action.type === "add-to-cart") {
+		const itemExists = state.cart.find(
+			(guitar) => guitar.id === action.payload.item.id
+		)
 
-        const itemExists = state.cart.findIndex(guitar => guitar.id === action.payload.item.id)
+		let updatedCart: CartItem[] = []
 
-        let updatedCart: CartItem[] = []
+		if (itemExists) {
+			// existe en el carrito
+			updatedCart = state.cart.map((item) => {
+				if (item.id === action.payload.item.id) {
+					if (item.quantity < MAX_ITEMS) {
+						return { ...item, quantity: item.quantity + 1 }
+					} else {
+						return item
+					}
+				} else {
+					return item
+				}
+			})
+		} else {
+			const newItem: CartItem = { ...action.payload.item, quantity: 1 }
+			updatedCart = [...state.cart, newItem]
+		}
 
-        if (itemExists >= 0) { // existe en el carrito
-            if (state.cart[itemExists].quantity >= MAX_ITEMS) return
-            updatedCart = [...state.cart]
-            updatedCart[itemExists].quantity++
-        } else {
-            const newItem: CartItem = { ...action.payload.item, quantity: 1 }
-            updatedCart = [...state.cart, newItem]
-        }
+		return {
+			...state,
+			cart: updatedCart,
+		}
+	}
 
-        return {
-            ...state,
-            updatedCart
-        }
-    }
+	if (action.type === "remove-from-cart") {
+		const updatedCart = state.cart.filter(
+			(item) => item.id !== action.payload.id
+		)
+		return {
+			...state,
+			cart: updatedCart,
+		}
+	}
 
-    if (action.type === "remove-from-cart") {
-        return {
-            ...state
-        }
-    }
+	if (action.type === "decrease-quantity") {
+		const cart = state.cart.map((item) => {
+			if (item.id === action.payload.id && item.quantity > MIN_ITEMS) {
+				return {
+					...item,
+					quantity: item.quantity - 1,
+				}
+			}
+			return item
+		})
 
-    if (action.type === "decrease-quantity") {
-        return {
-            ...state
-        }
-    }
+		return {
+			...state,
+			cart,
+		}
+	}
 
-    if (action.type === "increase-quantity") {
-        return {
-            ...state
-        }
-    }
+	if (action.type === "increase-quantity") {
+		const cart = state.cart.map((item) => {
+			if (item.id === action.payload.id && item.quantity < MAX_ITEMS) {
+				return {
+					...item,
+					quantity: item.quantity + 1,
+				}
+			}
+			return item
+		})
 
-    if (action.type === "clear-cart") {
-        return {
-            ...state
-        }
-    }
+		return {
+			...state,
+			cart,
+		}
+	}
 
-    return state
+	if (action.type === "clear-cart") {
+		return {
+			...state,
+			cart: [],
+		}
+	}
+
+	return state
 }
 
